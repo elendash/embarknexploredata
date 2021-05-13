006746ef1f3965ddac3cccfd2b36252fa389e21f
Eleanor <Eleanor>
2021-05-13 14:57:18 +0800

    update user

--- config/database.js
+++ config/database.js
@@ -1,13 +1,15 @@
+
 module.exports = ({ env }) => ({
+
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'embarknexplore.issrl.mongodb.net'),
+        host: env('DATABASE_HOST', "@embarknexplore.issrl.mongodb.net"),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'embarknexplore'),
+        database: env('DATABASE_NAME', "embarknexplore"),
         username: env('DATABASE_USERNAME', 'admin'),
         password: env('DATABASE_PASSWORD', 'adminadmin'),
       },
